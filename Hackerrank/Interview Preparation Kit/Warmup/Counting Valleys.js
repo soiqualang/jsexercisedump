80b37cb8991c4babcdf64b3eca8ac8decc7ee092
VeryEvilTomato <VeryEvilTomato@users.noreply.github.com>
2018-09-30 14:54:41 -0400

    forgot something

--- Hackerrank/Interview Preparation Kit/Warmup/Counting Valleys.js
+++ Hackerrank/Interview Preparation Kit/Warmup/Counting Valleys.js
@@ -1,14 +1,15 @@
-function jumpingOnClouds(c) {
-    let jumps = 0;
-    let i = 0;
-    while(i < c.length-1){
-    	if(c[i+1] == 1 || c[i+2] == 0) i += 2;
-      else i++;
-      jumps++
-    }
-    return jumps
+function countingValleys(n, s){
+  let level = 0;
+  let valleyCount = 0;
+  for(let i=0; i<n; i++){
+      if(s[i] == 'U') level++
+      else level--
+      if((level == 0) && (s[i] == 'U')) valleyCount++
+  }
+  return valleyCount
 }
 
-const c = [0,0,1,0,0,1,0]
+const n = 7
+const s = 'UDDDDUU'
 
-console.log(jumpingOnClouds(c))
+countingValleys(n,s)
